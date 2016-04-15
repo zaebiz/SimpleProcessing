d2dd758f3b956f63d679d6c6ce183c5dc6759f32
zaebiz <nikita.titarenko@gmail.com>
2016-04-15 16:05:17 +0300

    + TestClient prj

---
+++ SimpleProcessing.TestClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleProcessing.Models.Orders;
+using SimpleProcessing.Models.Cards;
+
+namespace SimpleProcessing.TestClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+
+		static void TestIncorrectRequestData()
+		{
+			PayOrderDto incorrectName = new PayOrderDto()
+			{
+				AmountKop = 100000,
+				OrderId = "123",
+				CardInfo = new CreditCardStandartInfo
+				{
+					CardholderName = "Nikita Titarenko Error",
+					CVVCode = "333",
+					CardNumber = "3333333333333333",
+					ExpireMonth = 4,
+					ExpireYear = 4
+				}
+			}
+		}
+	}
+}
