009a8aa30e3f4c7b990c30ca564cc9038d958854
zaebiz <nikita.titarenko@gmail.com>
2016-04-15 16:54:39 +0300

    + develop test client

--- SimpleProcessing.TestClient/Program.cs
+++ SimpleProcessing.TestClient/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using SimpleProcessing.Models.Orders;
 using SimpleProcessing.Models.Cards;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace SimpleProcessing.TestClient
 {
@@ -12,11 +14,14 @@ namespace SimpleProcessing.TestClient
 	{
 		static void Main(string[] args)
 		{
+			var task = TestIncorrectRequestData();
+			task.Wait();
+			Console.ReadKey();
 		}
 
-		static void TestIncorrectRequestData()
+		static async Task TestIncorrectRequestData()
 		{
-			PayOrderDto incorrectName = new PayOrderDto()
+			PayOrderDto incorrectRequest = new PayOrderDto()
 			{
 				AmountKop = 100000,
 				OrderId = "123",
@@ -28,6 +33,34 @@ namespace SimpleProcessing.TestClient
 					ExpireMonth = 4,
 					ExpireYear = 4
 				}
+			};
+
+			await PayRequest(incorrectRequest);
+		}
+
+		static async Task PayRequest(PayOrderDto req)
+		{
+			string reqContent = JsonConvert.SerializeObject(req);
+			Console.WriteLine(reqContent);
+
+			using (HttpClient client = new HttpClient())
+			{
+				//StringContent content = 
+				var request = new HttpRequestMessage()
+				{
+					RequestUri = new Uri("http://localhost:10393/api/processing/pay"),
+					Method = HttpMethod.Post,
+					Content = new StringContent(reqContent, Encoding.UTF8, "application/json")
+				};
+
+				request.Headers.Add("AuthorizationToken", "SecretKey123");
+				var response = await client.SendAsync(request);
+
+				//var response = await client.PostAsync("http://localhost:10393/api/processing/pay", content);
+				//response.EnsureSuccessStatusCode();
+
+				string data = await response.Content.ReadAsStringAsync();
+				Console.WriteLine(data);
 			}
 		}
 	}
